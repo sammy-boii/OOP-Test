8dc6b6f1ec68767fe7191ff76afd45168a2f9c8c
sammy-boii <neupanesamrajya2005@gmail.com>
2025-01-14 07:49:47 +0545

    Added getter methods to Student class

---
+++ Main.java
@@ -0,0 +1,16 @@
+package ws8;
+
+public class Main {
+	
+	String name;
+	int age;
+	
+	public Main(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public static void main(String [] args) {
+		
+	}
+}
